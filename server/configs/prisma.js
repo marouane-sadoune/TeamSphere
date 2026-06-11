e9daede02ace1487d6d91a8418a4bf86e8057c24
marouane-sadoune <marouanesadoune@gmail.com>
2026-06-11 16:24:12 +0100

    Refactor Prisma connection handling and integrate Inngest functions for user events

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -13,7 +13,10 @@ neonConfig.webSocketConstructor = ws;
 //var prisma: PrismaClient | undefined
 // }
 
-const connectionString = '${process.env.DATABASE_URL}';
+const connectionString = process.env.DATABASE_URL;
+if (!connectionString) {
+	throw new Error('DATABASE_URL is not set');
+}
 const adapter = new PrismaNeon ({ connectionString });
 const prisma = global.prisma || new PrismaClient({ adapter });
 if (process.env.NODE_ENV === 'development') global.prisma = prisma;
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,7 +1,54 @@
 import { Inngest } from "inngest";
+import prisma from "../configs/prisma.js";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({ id: "TeamSphere" });
 
-// Create an empty array where we'll export future Inngest functions
-export const functions = [];
+const syncUserCreation = inngest.createFunction( 
+    { id: "sync-user-with-clerk", triggers: { event: "clerk/user.created" } },
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.create({
+            data: {
+                id: data.id,
+                email: data?.email_addresses[0]?.email_address,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url,
+            },
+        });
+        console.log("User created with ID:", data.id);
+    }
+);
+
+const syncUserDeletion = inngest.createFunction(
+    { id: "delete-user-with-clerk", triggers: { event: "clerk/user.deleted" } },
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.delete({
+            where: {
+                id: data.id,
+            },
+        });
+        console.log("User deleted with ID:", data.id);
+    }
+);
+
+const syncUserUpdate = inngest.createFunction(
+    { id: "update-user-with-clerk", triggers: { event: "clerk/user.updated" } },
+    async ({ event }) => {
+        const { data } = event;
+        await prisma.user.update({
+            where: {
+                id: data.id,
+            },
+            data: {
+                email: data?.email_addresses[0]?.email_address,
+                name: data?.first_name + " " + data?.last_name,
+                image: data?.image_url,
+            },
+        });
+        console.log("User updated with ID:", data.id);
+    }
+);
+
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdate];
